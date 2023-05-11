f7d028054cf553533234b573cd570f1fc03e9ed4
WarrenT <warrentyler1024@gmail.com>
2023-05-11 14:49:50 +0100

    Add max speed, acceleration and friction

--- scripts/modules/car.js
+++ scripts/modules/car.js
@@ -7,16 +7,59 @@ export class Car {
     this.width = width;
     this.height = height;
 
+    this.maxSpeed = 3;
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.friction = 0.05;
+
     this.controls = new Controls();
   }
+  // update() {
+  //   if (this.controls.forward) {
+  //     this.speed += this.acceleration;
+  //   }
+  //   if (this.controls.backward) {
+  //     this.speed -= this.acceleration;
+  //   }
+
+  //   if (this.speed > this.maxSpeed) {
+  //     this.speed = this.maxSpeed;
+  //   }
+  //   if (this.speed < -this.maxSpeed / 2) {
+  //     this.speed = -this.maxSpeed / 2;
+  //   }
+    
+  //   if (this.speed > 0) {
+  //     this.speed -= this.friction;
+  //   }
+  //   if (this.speed < 0) {
+  //     this.speed += this.friction;
+  //   }
+
+
+  //   this.y -= this.speed;
+  // }
   update() {
-    if (this.controls.forward) {
-      this.y -= 2;
+    const { forward, backward } = this.controls;
+  
+    if (forward) {
+      this.speed += this.acceleration;
     }
-    if (this.controls.backward) {
-      this.y += 2;
+    if (backward) {
+      this.speed -= this.acceleration;
     }
+  
+    this.speed = Math.max(-this.maxSpeed / 2, Math.min(this.speed, this.maxSpeed));
+  
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    } else if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+  
+    this.y -= this.speed;
   }
+  
   /**
    * Draws the Car on the given canvas context.
    * @param {CanvasRenderingContext2D} ctx - The canvas context to draw on.
