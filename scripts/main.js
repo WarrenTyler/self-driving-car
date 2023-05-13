0e022ae8337dae08a2341ea2e758fefacaa8fb03
WarrenT <warrentyler1024@gmail.com>
2023-05-13 22:10:24 +0100

    Create road that moves depending on car movement

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { Car } from "./modules/car.js";
+import { Road } from "./modules/road.js";
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.querySelector("#canvas");
@@ -8,7 +9,8 @@ canvas.width = 200;
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -20,7 +22,15 @@ function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   car.update();
+
+  ctx.save();
+  const carScreenPosition = -car.y + canvas.height * 0.7;
+  ctx.translate(0, carScreenPosition);
+
+  road.draw(ctx);
   car.draw(ctx);
 
+  ctx.restore();
+  
   requestAnimationFrame(animate);
 }
