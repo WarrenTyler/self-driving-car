4ea0434ce34c1381ee3d3c69317989d5ad1dadbf
WarrenT <warrentyler1024@gmail.com>
2023-09-05 19:44:52 +0100

    Improve Code Clarity and Maintainability in Car Class
    - Refactored movement logic for better readability and added comments.
    
    - Maintained drawing logic with added comments for clarity.
    
    These changes enhance the Car class's code quality and structure without altering core functionality.

--- scripts/modules/car.js
+++ scripts/modules/car.js
@@ -20,42 +20,39 @@ export class Car {
   }
 
   #move() {
-    if (this.controls.forward) {
-      this.speed += this.acceleration;
-    }
-    if (this.controls.backward) {
-      this.speed -= this.acceleration;
+    // Calculate acceleration based on control inputs (forward/backward)
+    const acceleration = this.controls.forward
+      ? this.acceleration
+      : this.controls.backward
+      ? -this.acceleration
+      : 0;
+
+    // Apply acceleration to the speed
+    this.speed += acceleration;
+
+    // Ensure speed stays within the specified range
+    this.speed = Math.max(
+      -this.maxSpeed / 2,
+      Math.min(this.maxSpeed, this.speed)
+    );
+
+    // If there's a non-zero speed, adjust the car's angle
+    if (this.speed !== 0) {
+      // Determine the direction of the speed (forward/backward)
+      const flip = Math.sign(this.speed);
+
+      // Adjust the angle based on left/right controls
+      this.angle +=
+        0.03 * flip * (this.controls.left ? 1 : this.controls.right ? -1 : 0);
     }
 
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-    if (this.speed < -this.maxSpeed / 2) {
-      this.speed = -this.maxSpeed / 2;
-    }
+    // Apply friction to slow down the car
+    this.speed -= Math.sign(this.speed) * this.friction;
 
-    if (this.speed > 0) {
-      this.speed -= this.friction;
-    }
-    if (this.speed < 0) {
-      this.speed += this.friction;
-    }
+    // If the speed is very close to zero, set it to zero to prevent drift
+    this.speed = Math.abs(this.speed) < this.friction ? 0 : this.speed;
 
-    if (Math.abs(this.speed) < this.friction) {
-      this.speed = 0;
-    }
-
-    if (this.speed != 0) {
-      const flip = this.speed > 0 ? 1 : -1;
-
-      if (this.controls.left) {
-        this.angle += 0.03 * flip;
-      }
-      if (this.controls.right) {
-        this.angle -= 0.03 * flip;
-      }
-    }
-
+    // Update the car's position based on its angle and speed
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
   }
@@ -65,15 +62,21 @@ export class Car {
    * @param {CanvasRenderingContext2D} ctx - The canvas context to draw on.
    */
   draw(ctx) {
+    // Save the current state of the canvas context
     ctx.save();
 
+    // Translate the canvas to the car's position
     ctx.translate(this.x, this.y);
+
+    // Rotate the canvas to match the car's angle (negative angle to rotate clockwise)
     ctx.rotate(-this.angle);
 
+    // Begin a path and draw a filled rectangle representing the car
     ctx.beginPath();
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
 
+    // Restore the canvas to its previous state (undo the translation and rotation)
     ctx.restore();
   }
 }
