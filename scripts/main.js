f615b10497b0648e0649d0e82462027c4642c140
WarrenT <warrentyler1024@gmail.com>
2023-09-07 19:21:21 +0100

    Refactor Canvas Animation Loop for Clarity and Comments
    - Improved readability by adding comments and refactoring variable names for better understanding of the animation loop.
    
    - Added comments to describe the purpose of different code sections, making it more comprehensible.
    
    - Renamed the variable `carScreenPosition` to `carVerticalPosition` to better reflect its role in representing the car's vertical position on the screen.
    
    These changes make the canvas animation loop code more clear and easier to follow, with descriptive comments for each part of the process.

--- scripts/main.js
+++ scripts/main.js
@@ -9,28 +9,44 @@ canvas.width = 200;
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
+// Create a Road instance with a center at the bottom of the canvas
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
+
+// Create a Car instance positioned at the center of the first lane (lane index 1)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
+// Animation loop
 animate();
 
 function animate() {
+  // Adjust canvas height to match the viewport height
   const viewportHeight = window.innerHeight;
   if (canvas.height !== viewportHeight) {
     canvas.height = viewportHeight;
   }
+
+  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Update car position and properties
   car.update();
 
+  // Save the canvas state
   ctx.save();
-  const carScreenPosition = -car.y + canvas.height * 0.7;
-  ctx.translate(0, carScreenPosition);
 
+  // Calculate the vertical position of the car on the screen
+  const carVerticalPosition = -car.y + canvas.height * 0.7;
+
+  // Translate the canvas to adjust for the car's vertical position on the screen
+  ctx.translate(0, carVerticalPosition);
+
+  // Draw the road and car on the canvas
   road.draw(ctx);
   car.draw(ctx);
 
+  // Restore the canvas state
   ctx.restore();
-  
+
+  // Request the next animation frame
   requestAnimationFrame(animate);
 }
